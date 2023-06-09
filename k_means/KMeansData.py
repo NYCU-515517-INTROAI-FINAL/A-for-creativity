e9764fb04cd4c3407a294379e0caeef537430707
KWayne <summerblog9704@gmail.com>
2023-06-09 15:10:58 +0800

    add read dataset function in KMeansData & update main

--- k_means/KMeansData.py
+++ k_means/KMeansData.py
@@ -1,3 +1,11 @@
+import pandas as pd
+
 class KMeansData:
     def __init__(self):
+        self.dataset_link = "https://drive.google.com/uc?id=1wwGiv96gPqP9zu2CpJiaM_vePVl1v3bJ&export=download"
+        self.dataset = pd.DataFrame()
+        return
+    
+    def _read_dataset(self):
+        self.dataset = pd.read_csv(self.dataset_link)
         return
--- k_means/main.py
+++ k_means/main.py
@@ -1,4 +1,10 @@
+from KMeansData import KMeansData
 def main():
+    print("Setting link of dataset")
+    k_means_data = KMeansData()
+    
+    print("Reading dataset")
+    k_means_data._read_dataset()
     return
 
 if __name__ == '__main__':
