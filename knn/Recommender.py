b9c335ab6c484612c362308eab7f8cece1c8966a
KWayne <summerblog9704@gmail.com>
2023-06-09 14:52:58 +0800

    update main and Recommender class

--- knn/Recommender.py
+++ knn/Recommender.py
@@ -1,3 +1,18 @@
+from sklearn.neighbors import NearestNeighbors
+
 class Recommender:
-    def __init__(self):
+    def __init__(self, metric, algorithm, k, data):
+        self.metric = metric
+        self.algorithm = algorithm
+        self.k = k
+        self.data = data
+        self.model = None
         return
+    
+    def make_recommendation(self, song_id, n_recommendations):
+        recommendations = list()
+        self.model = NearestNeighbors(metric=self.metric, algorithm=self.algorithm, n_neighbors=self.k, n_jobs=-1).fit(self.data)
+        distances, indices = self.model.kneighbors(self.data[song_id], n_neighbors=n_recommendations+1)
+        for i in range(1, len(distances[0])):
+            recommendations.append([distances[0][i], indices[0][i]])
+        return recommendations
--- knn/main.py
+++ knn/main.py
@@ -1,6 +1,67 @@
 from KNNData import KNNData
+from Recommender import Recommender
+from tabulate import tabulate
 
 def main():
+    knn_data = KNNData()
+    
+    print("Reading dataset")
+    knn_data._read_file()
+    
+    print("Setting up variables and data")
+    knn_data._setup()
+    
+    print("Decoding dataset")
+    knn_data._decode()
+    
+    k = int(input("Enter a number K for KNN algorithm: "))
+    print("Making recommender")
+    recommender = Recommender(metric='cosine', algorithm='brute', k=k, data=knn_data.get_matrix_songs_features())
+    
+    try:
+        # detect if fuzzy wuzzy cannot find the song input by user
+        song_id, n_recommendations = prompt(knn_data=knn_data)
+    except:
+        print("Program is going to terminate")
+        return
+    
+    print(f"Starting the recommendation process...")
+    song_name, song_artist, recommendations = make_recommendations(song_id=song_id, n_recommendations=n_recommendations, knn_data=knn_data, recommender=recommender)
+    print_recommendations(song_name=song_name, song_artist=song_artist, recommendations=recommendations)
+    return
+
+def prompt(knn_data: KNNData):
+    is_pass = False
+    while not is_pass:
+        song = str(input("Enter a song that you would like to get recommendation based on: "))
+        try:
+            found_song_id = knn_data.fuzzy_matching(song=song)
+        except:
+            print(f"The recommendation system could not find a match for {song} in dataset.")
+            raise
+        found_song = knn_data.get_decode_song()[found_song_id]
+        if song != found_song:
+            user_input = str(input(f"Is the song you are looking for \"{found_song}\"? (y for yes) "))
+            if  user_input == 'y' or user_input == 'Y':
+                is_pass = True
+        else:
+            is_pass = True
+    n_recommendations=int(input("Enter the number of recommendations you would like: "))
+    return found_song_id, n_recommendations
+
+def make_recommendations(song_id: int, n_recommendations: int, knn_data: KNNData, recommender: Recommender):
+    recommendations = list()
+    recommended_id_list = recommender.make_recommendation(song_id=song_id, n_recommendations=n_recommendations)
+
+    for index, songs_id_list in enumerate(recommended_id_list):
+        recommendations.append([index, knn_data.get_decode_song()[songs_id_list[1]], knn_data.get_decode_artist()[songs_id_list[1]], 1 - songs_id_list[0]])
+
+    song_name, song_artist = knn_data.get_decode_song()[song_id], knn_data.get_decode_artist()[song_id]
+    return song_name, song_artist, recommendations
+
+def print_recommendations(song_name, song_artist, recommendations):
+    print(f"The recommendations for {song_name} from {song_artist} are:")
+    print(tabulate(recommendations, headers=["#", "Song Name", "Artist", "Similarity"], floatfmt=".3f"))
     return
 
 if __name__ == '__main__':
