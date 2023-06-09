e37492b7da34b96c3a756faa99a1dd09e987b94b
KWayne <summerblog9704@gmail.com>
2023-06-09 15:37:30 +0800

    add KMeans algorithm in train function in KMeansData & update main

--- k_means/KMeansData.py
+++ k_means/KMeansData.py
@@ -1,11 +1,35 @@
 import pandas as pd
+import numpy as np
+from sklearn.cluster import KMeans
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import Pipeline
 
 class KMeansData:
     def __init__(self):
+        # initialize the variables list
         self.dataset_link = "https://drive.google.com/uc?id=1wwGiv96gPqP9zu2CpJiaM_vePVl1v3bJ&export=download"
         self.dataset = pd.DataFrame()
+        
+        # be assigned in train function
+        self.song_cluster_pipeline = None
+        self.X = None
+        self.song_cluster_labels = None
         return
     
     def _read_dataset(self):
         self.dataset = pd.read_csv(self.dataset_link)
         return
+    
+    def _train(self, n_clusters):
+        self.song_cluster_pipeline = Pipeline([("scaler", StandardScaler()), 
+                                               ("kmeans", KMeans(n_clusters=n_clusters, verbose=False))], verbose=False)
+        X = self.dataset.select_dtypes(np.number)
+        self.song_cluster_pipeline.fit(X)
+        self.song_cluster_labels = self.song_cluster_pipeline.predict(X)
+        return
+    
+    def get_X(self):
+        return self.X
+    
+    def get_song_cluster_labels(self):
+        return self.song_cluster_labels
--- k_means/main.py
+++ k_means/main.py
@@ -1,10 +1,14 @@
 from KMeansData import KMeansData
+
 def main():
     print("Setting link of dataset")
     k_means_data = KMeansData()
     
     print("Reading dataset")
     k_means_data._read_dataset()
+    
+    print("Training model")
+    k_means_data._train(n_clusters=20)
     return
 
 if __name__ == '__main__':
